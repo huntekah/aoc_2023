5284907ea6d218a62da4191fa9a60bf5fa3ed090
huntekah <huntekah@gmail.com>
2023-12-09 23:55:02 +0100

    task 3.1

---
+++ aoc_3/aoc_3_2.py
@@ -0,0 +1,91 @@
+"""If you can add up all the part numbers in the engine schematic, it should be easy to work out which part is missing.
+
+The engine schematic (your puzzle input) consists of a visual representation of the engine. There are lots of numbers and symbols you don't really understand, but apparently any number adjacent to a symbol, even diagonally, is a "part number" and should be included in your sum. (Periods (.) do not count as a symbol.)
+
+Here is an example engine schematic:
+
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+
+A gear is any * symbol that is adjacent to exactly two part numbers. Its gear ratio is the result of multiplying those two numbers together.
+
+This time, you need to find the gear ratio of every gear and add them all up so that the engineer can figure out which gear needs to be replaced."""
+
+
+def read_puzzle():
+    with open("input.txt", "r") as f:
+    # with open("small_input.txt", "r") as f:
+        return f.read().splitlines()
+
+
+def solve_puzzle(puzzle):
+    result = 0
+    for i, line in enumerate(puzzle):
+        result += get_sum_of_adjacent_numbers(line, puzzle, i)
+    return result
+
+
+def get_sum_of_adjacent_numbers(line, puzzle, i):
+    result = 0
+    number = 0
+    is_adjacent = False
+    for j, char in enumerate(line):
+        if char.isdigit():
+            number = number * 10 + int(char)
+            if not is_adjacent:
+                is_adjacent = check_if_adjacent(puzzle, i, j)
+        else:
+            if is_adjacent:
+                print(f"OK {number}")
+                result += number
+            elif number != 0:
+                print(f"BAD {number}")
+            number = 0
+            is_adjacent = False
+    else:
+        if is_adjacent:
+            print(f"OK {number}")
+            result += number
+    return result
+
+
+def check_if_adjacent(puzzle, i, j):
+    fields_to_check = [
+        (i - 1, j - 1),
+        (i - 1, j),
+        (i - 1, j + 1),
+        (i, j - 1),
+        (i, j + 1),
+        (i + 1, j - 1),
+        (i + 1, j),
+        (i + 1, j + 1),
+    ]
+    for field in fields_to_check:
+        if (
+            field[0] >= 0
+            and field[1] >= 0
+            and field[0] < len(puzzle)
+            and field[1] < len(puzzle[0])
+        ):
+            if check_field(field, puzzle):
+                return True
+
+
+def check_field(field, puzzle):
+    i, j = field
+    check = (not puzzle[i][j].isdigit()) and (puzzle[i][j] != ".")
+    print(f"Checking {i} {j} -> {puzzle[i][j]} is {check}")
+    return check
+
+
+if __name__ == "__main__":
+    puzzle = read_puzzle()
+    print(solve_puzzle(puzzle))
