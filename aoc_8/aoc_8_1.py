66a502db582305f68e478aa842f902bbae40b42f
Krzysztof Jurkiewicz <krzysztof.jurkiewicz@zowie.ai>
2023-12-18 08:53:35 +0100

    feature: solve task 7.1

--- aoc_8/aoc_8_1.py
+++ aoc_8/aoc_8_1.py
@@ -7,6 +7,8 @@ Directions = list[int]
 # Nodes is a Dict with two nodes like node['AAA'] = ['BBB', 'CCC']
 Node = str
 Nodes = dict[Node, list[Node]]
+
+
 def solve_puzzle(puzzle):
     directions, nodes = transform_puzzle(puzzle)
     next_node = "AAA"
@@ -15,6 +17,7 @@ def solve_puzzle(puzzle):
         next_node = traverse_graph(directions, nodes, next_node, steps)
         steps += 1
     return steps
+
 
 def transform_puzzle(puzzle) -> tuple[Directions, Nodes]:
     directions: Directions = letters_to_directions(puzzle[0])
@@ -27,9 +30,11 @@ def transform_puzzle(puzzle) -> tuple[Directions, Nodes]:
             nodes[node] = children
     return directions, nodes
 
+
 def letters_to_directions(line: str) -> Directions:
     # 0 means L and 1 means R
     return [0 if c == "L" else 1 for c in line]
+
 
 def read_node(line: str) -> tuple[str, list[str]]:
     # read line like "AAA = (BBB, CCC)" to tuple("AAA", ["BBB", "CCC"])
@@ -38,6 +43,7 @@ def read_node(line: str) -> tuple[str, list[str]]:
     children = children.strip()[1:-1].split(",")
     children = [child.strip() for child in children]
     return node, children
+
 
 def traverse_graph(directions: Directions, nodes: Nodes, node: str, steps: int) -> Node:
     # if node == "ZZZ":
--- util/process_input.py
+++ util/process_input.py
@@ -1,10 +1,10 @@
 import re
 
 
-def read_puzzle(input_file="input.txt"):
+def read_puzzle(input_file: str = "input.txt") -> list[str]:
     with open(input_file, "r") as f:
         return f.read().splitlines()
 
 
-def string_of_numbers_to_list(line):
+def string_of_numbers_to_list(line: str) -> list[int]:
     return list(map(int, re.sub(" +", " ", line).strip().split(" ")))
